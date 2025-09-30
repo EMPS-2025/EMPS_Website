60a98bf29eedd4e13c45be4981c082d1a8040630
EMPS-2025 <marketing@energyminds.in>
2025-09-30 07:08:13 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,14 @@ import Footer from './components/Footer';
 
 import './globals.css'; // This is where your styles will go
 
+export const metadata = {
+  title: "Energyminds Power Solutions",
+  description: "Technology-integrated energy trading platform delivering data-driven insights and sustainable energy management.",
+  icons: {
+    icon: "/favicon_EMPS.png",   // or "/ENERGY-MINDS-LOGO.png"
+  },
+};
+
 export default function RootLayout({children}) {
   return(
     <html lang="en">
