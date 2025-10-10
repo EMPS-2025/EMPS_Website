1dd8506bf13433f434c12fef4ca70c453388a39d
EMPS-2025 <marketing@energyminds.in>
2025-10-10 17:42:21 -0400

    Update layout.js 1

--- app/layout.js
+++ app/layout.js
@@ -2,33 +2,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 import './globals.css'; // This is where your styles will go
-
-export const metadata = {
-  useEffect(() => {
-    const btn = document.getElementById("scrollToTopBtn");
-    const toggleVisibility = () => {
-      if (window.scrollY > 300) {
-        btn.classList.add("visible");
-      } else {
-        btn.classList.remove("visible");
-      }
-    };
-    window.addEventListener("scroll", toggleVisibility);
-
-    btn.addEventListener("click", () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-
-    return () => {
-      window.removeEventListener("scroll", toggleVisibility);
-    };
-  }, []);
-  title: "Energyminds Power Solutions",
-  description: "Technology-integrated energy trading platform delivering data-driven insights and sustainable energy management.",
-  icons: {
-    icon: "/favicon_EMPS.png",   // or "/ENERGY-MINDS-LOGO.png"
-  },
-};
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 export default function RootLayout({children}) {
   return(
@@ -36,11 +10,10 @@ export default function RootLayout({children}) {
       <body>
         <Navbar />
           <main>{children}</main>
-    <button id="scrollToTopBtn" className="scroll-to-top-btn" aria-label="Scroll to top">
-  ↑
-</button>
+<ScrollToTopButton />
         <Footer />
       </body>
     </html>
   );
 }
+
